a9f32be9c656fb73f957c25e522d4fa3f70af367
Meshram Pratik Ganesh <112073612+Jerry2T@users.noreply.github.com>
2025-04-12 05:10:06 +0000

    m,

---
+++ osl/os6.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+using namespace std;
+
+void printallocation(const vector<int> &allocation, const vector<int> &processSize, vector<int> &blockSize) {
+    cout << "ProcessID\tProcessSize\tBlockID\tBlockSize\tFragmentedMemory\n";
+    for (int i = 0; i < processSize.size(); i++) {
+        if (allocation[i] != -1) {
+            cout << i + 1 << "\t\t" << processSize[i] << "\t\t" << allocation[i] + 1 << "\t\t" << blockSize[allocation[i]] + processSize[i] << "\t\t" << blockSize[allocation[i]] << endl;
+        } else {
+            cout << i + 1 << "\t\t" << processSize[i] << "\t\tNot Allocated\t-\t-" << endl;
+        }
+    }
+}
+
+void firstFit(vector<int> &blockSize, vector<int> &processSize) {
+    int n = processSize.size(), m = blockSize.size();
+    vector<int> allocation(n, -1);    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                allocation[i] = j;
+                blockSize[j] -= processSize[i];
+                break;
+            }
+        }
+    }    
+    cout << "First Fit Allocation:\n";
+    printallocation(allocation, processSize, blockSize);
+}
+
+void nextfit(vector<int> blocksize, vector<int> processsize) {
+    int n=processsize.size() ,m=blocksize.size(), lastallocated = 0;
+    vector<int> allocation(n, -1);
+    for(int i=0; i<n; i++) {
+        for(int j=lastallocated; j<m; j++) {
+            if(blocksize[j] >= processsize[i]) {
+                allocation[i] = j;
+                blocksize[j] -= processsize[i];
+                lastallocated = j;
+                break;
+            }
+        }
+    }
+    cout<<"Next Fit Allocation:\n";
+    printallocation(allocation, processsize, blocksize);
+}
+
+void worstFit(vector<int> &blockSize, vector<int> &processSize) {
+    int n = processSize.size(), m = blockSize.size();
+    vector<int> allocation(n, -1);    
+    for (int i = 0; i < n; i++) {
+        int worstIdx = -1;
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                if(worstIdx== -1 || blockSize[j]>blockSize[worstIdx]) {
+                    worstIdx = j;
+                }
+            }
+        }
+        if (worstIdx != -1) {
+            allocation[i] = worstIdx;
+            blockSize[worstIdx] -= processSize[i];
+        }
+    }    
+    cout << "Worst Fit Allocation:\n";
+    printallocation(allocation, processSize, blockSize);
+}
+
+void bestFit(vector<int> &blockSize, vector<int> &processSize) {
+    int n = processSize.size(), m = blockSize.size();
+    vector<int> allocation(n, -1);    
+    for (int i = 0; i < n; i++) {
+        int bestIdx = -1;
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                if (bestIdx == -1 || blockSize[j] < blockSize[bestIdx]) {
+                    bestIdx = j;
+                }
+            }
+        }
+        if (bestIdx != -1) {
+            allocation[i] = bestIdx;
+            blockSize[bestIdx] -= processSize[i];
+        }
+    }    
+    cout << "Best Fit Allocation:\n";
+    printallocation(allocation, processSize, blockSize);
+}
+
+
+int main(){
+    int choice, numblocks, numprocesses;
+    vector<int> blocksize, processsize;
+    cout<<"Enter the number of blocks: ";
+    cin>>numblocks;
+
+    cout << "Enter sizes for " << numblocks << " blocks: ";
+    for (int i = 0; i < numblocks; i++) {
+        int size;
+        cin >> size;
+        blocksize.push_back(size);
+    }    
+    cout << "Enter number of processes: ";
+    cin >> numprocesses;
+    cout << "Enter sizes for " << numprocesses << " processes: ";
+    for (int i = 0; i < numprocesses; i++) {
+        int size;
+        cin >> size;
+        processsize.push_back(size);
+    }
+
+    cout << "Choose a Memory Allocation Strategy:\n";
+    cout << "1. First Fit\n";
+    cout << "2. Next Fit\n";
+    cout << "3. Best Fit\n";
+    cout << "4. Worst Fit\n";
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch(choice) {
+        case 1: firstFit(blocksize, processsize);
+                break;
+        case 2: nextfit(blocksize, processsize);
+                break;
+        case 3: bestFit(blocksize, processsize);
+                break;
+        case 4: worstFit(blocksize, processsize);
+                break;
+        default: cout << "Invalid choice!" << endl;
+    }
+    return 0;
+}
--- osl/os8.cpp
+++ osl/os8.cpp
@@ -41,14 +41,63 @@ void scan(vector<int> request, int head,int diskSize ,string direction){
     vector<int> left, right, seekSequence;
     if (direction == "left") left.push_back(0);
     else right.push_back(diskSize - 1);
+    for(int r: request){
+        if(r < head) left.push_back(r);
+        else right.push_back(r);
+    }
+    
+    sort(left.begin(), left.end());
+    sort(right.begin(), right.end());
+    cout<<"Sequence of requests: ";
 
+    if(direction == "left"){
+        for(int i=left.size()-1; i>=0; i--) seekSequence.push_back(left[i]);
+        for(int i=0; i<right.size(); i++) seekSequence.push_back(right[i]);
+    } else {
+        for(int i=0; i<right.size(); i++) seekSequence.push_back(right[i]);
+        for(int i=left.size()-1; i>=0; i--) seekSequence.push_back(left[i]);
+    }
+   
+    for(int r: seekSequence){
+        cout<<r<<" ";
+        seek += abs(head - r);
+        head = r;
+    }
+    cout<<"\nTotal seek time: "<<seek<<endl;
+}
 
-    // Implement Scan here
+void cscan(vector<int> request, int head, int diskSize){
+    int seek = 0;
+    vector<int> left, right, seekSequence;
+    right.push_back(diskSize - 1);
+    left.push_back(0);
+    for(int r: request){
+        if(r < head) left.push_back(r);
+        else right.push_back(r);
+    }
+    sort(left.begin(), left.end());
+    sort(right.begin(), right.end());
+    cout<<"Sequence of requests: ";
+
+    for(int i=0; i<right.size(); i++) seekSequence.push_back(right[i]);
+    for(int i=0; i<left.size(); i++) seekSequence.push_back(left[i]);
+    for(int r: seekSequence){
+        cout<<r<<" ";
+        seek += abs(head - r);
+        head = r;
+        if(head == diskSize - 1){
+            seek += abs(diskSize - 1);
+            head = 0;
+        } 
+    }
+    cout<<"\nTotal seek time: "<<seek<<endl;
 }
 
 int main(){
-    int n, head, choice;
+    int n, head, choice, diskSize;
+    string direction;
     vector<int> request(n);
+
     cout<<"Enter the number of requests: ";
     cin>>n;    
     cout<<"Enter the requests: ";
@@ -57,12 +106,11 @@ int main(){
     }    
     cout<<"Enter the initial head position: ";
     cin>>head;
-
     cout<<"1: FCFS Disk Scheduling Algorithm\n";
     cout<<"2: SSTF Disk Scheduling Algorithm\n";
     cout<<"3: Scan Scheduling Algorithm\n";
     cout<<"4: C-Scan Disk Scheduling Algorithm\n";
-    cin>>choice;
+    cin>>choice; 
 
     switch (choice){
         case 1: cout<<"FCFS Disk Scheduling Algorithm\n";
@@ -72,13 +120,18 @@ int main(){
                 sstf(request, head);
                 break;
         case 3: cout<<"Scan Scheduling Algorithm\n";
-                // Implement Scan here
+                cout<<"Enter the disk size: ";
+                cin>>diskSize;
+                cout<<"Enter the direction (left/right): ";
+                cin>>direction;
+                scan(request, head, diskSize, direction);
                 break;
         case 4: cout<<"C-Scan Disk Scheduling Algorithm\n";
-                // Implement C-Scan here
+                cout<<"Enter the disk size: ";
+                cin>>diskSize;
+                cscan(request, head, diskSize);
                 break;
-    default:cout<<"Invalid choice"<<endl;                
+        default:cout<<"Invalid choice"<<endl;                
     }
-
     return 0;
 }
