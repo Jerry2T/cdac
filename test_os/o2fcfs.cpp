d776701116d038051654dfa55bdec27f371ca3bd
Meshram Pratik Ganesh <112073612+Jerry2T@users.noreply.github.com>
2025-04-21 08:33:53 +0000

    j

--- test_os/o2fcfs.cpp
+++ test_os/o2fcfs.cpp
@@ -47,6 +47,55 @@ void fcfs(vector<Process>& proc){
     printtable(false, proc);
 }
 
+void roundrobin(vector<Process>& proc, int quantum, int n) {
+    queue<int> q;
+    int time = 0, completed = 0;
+    vector<bool> visited(n, false);
+    sort(proc.begin(), proc.end(), [](const Process &a, const Process &b) { return a.arrival_time < b.arrival_time;});
+    for (int i = 0; i < n; i++)     proc[i].remaining_time = proc[i].burst_time;
+    q.push(0);
+    visited[0] = true;
+    
+    while (completed < n) {
+        if (q.empty()) {
+            for (int i = 0; i < n; i++) {
+                if (!visited[i] && proc[i].arrival_time <= time) {
+                    q.push(i);
+                    visited[i] = true;
+                    break;
+                }
+            }
+            if (q.empty()) {
+                time++;
+                continue;
+            }
+        }
+
+        int idx = q.front();
+        q.pop();
+        if (proc[idx].remaining_time > quantum) {
+            time += quantum;
+            proc[idx].remaining_time -= quantum;
+        } else {
+            time += proc[idx].remaining_time;
+            proc[idx].remaining_time = 0;
+            proc[idx].completion_time = time;
+            proc[idx].turnaround_time = proc[idx].completion_time - proc[idx].arrival_time;
+            proc[idx].waiting_time = proc[idx].turnaround_time - proc[idx].burst_time;
+            completed++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && proc[i].arrival_time <= time) {
+                q.push(i);
+                visited[i] = true;
+            }
+        }
+        if (proc[idx].remaining_time > 0)  q.push(idx);
+    }
+    printtable(false, proc);
+}
+
 int main(){
     int n;
     cout<<"Enter the number of processes: ";
@@ -55,11 +104,17 @@ int main(){
     cout<<"Enter the arrival time, burst time and priority of each process:\n";
     for(int i=0; i<n; i++){
         proc[i].id = i+1;
-        cin>>proc[i].arrival_time>>proc[i].burst_time>>proc[i].priority;
+        cin>>proc[i].arrival_time>>proc[i].burst_time;
         proc[i].remaining_time = proc[i].burst_time;
     }
     
-    cout<<"First Come First Serve Scheduling:\n";
-    fcfs(proc);
+    //cout<<"First Come First Serve Scheduling:\n";
+    //fcfs(proc);
+    cout<<"\nRound Robin Scheduling:\n";
+    int quantum;
+    cout<<"Enter the quantum time: ";
+    cin>>quantum;
+    roundrobin(proc, quantum,n);
+    cout<<"\n\n";
     return 0;
 }
